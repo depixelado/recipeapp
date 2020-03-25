ffb7f7d31ed6dbd63b717d9a4cb842e9f9666f4a
Ramon Mayor <Ramon.mayor.uk@gmail.com>
2020-03-25 14:15:14 +0100

    Setup project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
+
+ReactDOM.render(
+  <App title="Recipe" name="Ramon" />,
+  document.getElementById("root")
+);
