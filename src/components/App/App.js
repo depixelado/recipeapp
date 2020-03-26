64d093a5c7fe0cffc612de59e489c64a1a952ea2
Ramon Mayor <Ramon.mayor.uk@gmail.com>
2020-03-26 19:12:41 +0100

    Fetch results from edamam

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,31 +5,55 @@ import SearchBox from "../SearchBox/SearchBox";
 
 import "./styles.scss";
 
-function App() {
-  return (
-    <div className="app">
-      <header>
-        <img src="img/logo.png" />
-        <SearchBox />
-      </header>
-      <div className="recipeList">
-        <RecipeBox
-          label="Paparajote"
-          cookingTime={120}
-          calories={1200}
-          source="http://www.google.es"
-          dietType="Mediterranean"
-        />
-        <RecipeBox
-          label="Paella"
-          cookingTime={120}
-          calories={1200}
-          source="http://www.google.es"
-          dietType="Mediterranean"
-        />
+const url =
+  "https://api.edamam.com/search?q=chicken&app_id=4b656b44&app_key= 203ba625f4419c5c9f0253448a19fc41";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: []
+    };
+  }
+
+  loadRecipes() {
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ recipes: data.hits.map(hit => hit.recipe) });
+      });
+  }
+
+  componentDidMount() {
+    this.loadRecipes();
+  }
+
+  render() {
+    console.log("render", this.state.recipes);
+
+    return (
+      <div className="app">
+        <header>
+          <img src="img/logo.png" />
+          <SearchBox />
+        </header>
+        <div className="recipeList">
+          {this.state.recipes.map(recipe => (
+            <RecipeBox
+              image={recipe.image}
+              label={recipe.label}
+              cookingTime={recipe.totalTime}
+              calories={recipe.calories}
+              source={recipe.url}
+              dietType={recipe.dietLabels}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/RecipeBox/RecipeBox.js
+++ src/components/RecipeBox/RecipeBox.js
@@ -3,28 +3,35 @@ import Icon from "react-fontawesome";
 
 import "./styles.scss";
 
-function RecipeBox({ label, cookingTime, calories, source, dietType }) {
+function RecipeBox({ image, label, cookingTime, calories, source, dietType }) {
   return (
     <div className="recipeBox">
-      <div className="thumb"></div>
+      <div className="thumb">
+        <img src={image} />
+      </div>
       <div className="content">
         <div className="label">{label}</div>
         <div className="info">
           <div className="cookingTime">
             <Icon name="clock-o" />
-            <span> {cookingTime} Min</span>
+            <span>
+              {cookingTime != 0 ? ` ${cookingTime} Min` : " Not available"}
+            </span>
           </div>
           <div className="calories">
             <Icon name="fire" />
             <span> {calories} Kcal</span>
           </div>
-          <div className="dietType">
-            <Icon name="cutlery" />
-            <span> {dietType}</span>
-          </div>
+          {dietType && !!dietType.length && (
+            <div className="dietType">
+              <Icon name="cutlery" />
+              <span> {dietType}</span>
+            </div>
+          )}
+
           <div className="source">
             <Icon name="external-link" />
-            <span> {source}</span>
+            <a href={source}>Source</a>
           </div>
         </div>
       </div>
